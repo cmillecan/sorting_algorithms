24223fcd12330802c290925dcb30b712599a8233
cmillecan <cmillecan@gmail.com>
2020-06-18 01:25:49 +0000

    add file

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -9,19 +9,24 @@
  */
 void shell_sort(int *array, size_t size)
 {
-	unsigned int gap, i = 0, j = 0;
+	unsigned int gap = 1, i = 0, j = 0;
 	int temp;
 
-	for (gap = size / 2; gap > 0; gap = gap / 2)
+	while (gap <= size / 3)
+		gap = gap * 3 + 1;
+
+	while (gap > 0)
 	{
 		for (i = gap; i < size; i++)
 		{
 			temp = array[i];
 			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+			{
 				array[j] = array[j - gap];
-
+			}
 			array[j] = temp;
 		}
+		gap = (gap - 1) / 3;
 		print_array(array, size);
 	}
 }
